eee9b0ec750e17fb3cceffd54e986952496f456b
Subhojeet-Mukherjee <msubhojeet111@gmail.com>
2025-07-31 19:49:17 +0530

    folder upload

---
+++ src/script.js
@@ -0,0 +1,62 @@
+const apiKey="079d1cc431aebd0ccfdc05fbed16ee1f";
+const searchBox= document.querySelector("input");
+const searchBtn= document.querySelector("button");
+const weatherIcon=document.querySelector(".weather-icon")
+
+
+
+async function checkWeather(city) {
+    const apiURL=`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+        try {
+        const response= await fetch(apiURL);
+        const data= await response.json();
+        console.log(data.cod)
+        if (data.cod === "404" || data.cod=== "400") {
+            alert("City not found. Please enter a valid city name.");
+            
+            return;
+
+        }
+        else {
+            document.querySelector(".City").textContent= data.name;
+            document.querySelector(".temp").textContent= Math.round(data.main.temp) +"°C";
+            document.querySelector(".humidity").textContent= data.main.humidity+"%";
+            document.querySelector(".wind").textContent= data.wind.speed+" km/h";
+
+            switch (data.weather[0].main) {
+                case "Clouds":
+                    weatherIcon.src="images/clouds.png"
+                    
+                    break;
+                case "Mist":
+                    weatherIcon.src="images/mist.png"
+                 break;
+                case "Clear":
+                    weatherIcon.src="images/clear.png"
+                    break;
+                case "Rain":
+                    weatherIcon.src="images/rain.png"
+                    break;
+                case "Drizzle":
+                    weatherIcon.src="images/drizzle.png"
+                    break;
+            }
+    
+        }
+    } catch (error) {
+        alert("Error in fetching details",error)
+    }
+   
+}
+
+checkWeather("assam");
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+},false)
+
+searchBox.addEventListener("keydown",(event)=>{
+    if(event.key =="Enter"){   
+    checkWeather(searchBox.value);
+}
+},false)
